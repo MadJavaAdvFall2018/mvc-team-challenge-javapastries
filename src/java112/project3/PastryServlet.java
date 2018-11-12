265c2061347c8ad450a13bd0fd22d836320946b5
Kat Kohler <kkohler@madisoncollege.edu>
2018-11-12 15:39:32 -0600

    servlet -adding documentation

--- src/java112/project3/PastryServlet.java
+++ src/java112/project3/PastryServlet.java
@@ -13,6 +13,12 @@ import java.io.IOException;
         urlPatterns = { "/Pastry", "/PastryServlet" }
 )
 
+/**
+ *  This servlet is the Controller for the "What Pastry Personality Are You?" application. It demonstrates use of
+ *  MVC architecture and the flow between servlet (controller), JavaBean (model), and JSP (view). The "What Pastry
+ *  Personality Are You?" app asks questions of the user and, based on the user response, determines the user's
+ *  pastry personality, answering the age-old question "what kind of a pastry would if you were a pastry?".
+ */
 public class PastryServlet extends HttpServlet {
 
     /**
@@ -25,12 +31,15 @@ public class PastryServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //setting up variables for use
         Pastry yourPastry = new Pastry();
-        String url = "/PastryJSP.jsp";
+        String url = "/PastryJSP.jsp";      //the URL to send/forward browser to
 
-
+        //setting up attributes
         request.setAttribute();
 
+
+        //forwarding browser to output JSP (a forward since we don't want user to be able to bookmark this page)
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
     }
