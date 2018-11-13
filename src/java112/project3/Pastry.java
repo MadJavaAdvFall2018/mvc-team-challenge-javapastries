53b3eb68f3c6281f6928d97667bc1786866d868a
Kat Kohler <kkohler@madisoncollege.edu>
2018-11-12 18:05:55 -0600

    pastry bean - initial bean/Pastry class commit (fields & getters and setters only);

---
+++ src/java112/project3/Pastry.java
@@ -0,0 +1,33 @@
+package java112.project3;
+
+public class Pastry {
+
+    private int od;
+    private boolean nuts;
+    private String flavor;
+
+    public int getOd() {
+        return od;
+    }
+
+    public void setOd(int od) {
+        this.od = od;
+    }
+
+    public boolean isNuts() {
+        return nuts;
+    }
+
+    public void setNuts(boolean nuts) {
+        this.nuts = nuts;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+}
