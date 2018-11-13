0d6bc91024d13b0759ad0ae8aea0c70e26c3bc7b
Heidi Aandahl <hlaandahl@madisoncollege.edu>
2018-11-12 22:16:18 -0600

    added methods and more instance variables to Pastry.java; no testing or javadoc yet

--- src/java112/project3/Pastry.java
+++ src/java112/project3/Pastry.java
@@ -3,8 +3,40 @@ package java112.project3;
 public class Pastry {
 
     private int od;
-    private boolean nuts;
+    private boolean nuts;  //allergy
     private String flavor;
+    private String pastryName;
+    private String pastryMessage;
+/*
+int od;  How many times have you overdosed on pastries in the past week? 0; 1-2; 3-5; 6+
+bool nuts; Are you allergic to nuts? y / n
+String flavor; cherry; ~apple~ lemon; chocolate; vanilla; (edited)
+
+OD: 0-2 whatever, 3+ Death By Chocolate,
+Nutallergy - whatever, no nut allergy whatever
+Flavors: one option each for no-OD, one for OD
+
+*/
+
+
+    /**
+     * Empty constructor for Pastry object
+     */
+    public Pastry() {
+        // TODO default stuff??
+    }
+
+    /**
+     * Constructor for Pastry object with all web user answers included
+     * @param od how often the user overdosed on pastries
+     * @param nuts whether the user has a nuts allergy
+     * @param flavor user's favorite flavor
+     */
+    public Pastry (int od, boolean nuts, String flavor) {
+        this.od = od;
+        this.nuts = nuts;
+        this.flavor = flavor;
+    }
 
     public int getOd() {
         return od;
@@ -28,6 +60,67 @@ public class Pastry {
 
     public void setFlavor(String flavor) {
         this.flavor = flavor;
+    }
+
+    public String getPastryName() {
+        return pastryName;
+    }
+
+    public void setPastryName(String pastryName) {
+        this.pastryName = pastryName;
+    }
+
+    public String getPastryMessage() {
+        return pastryMessage;
+    }
+
+    public void setPastryMessage(String pastryMessage) {
+        this.pastryMessage = pastryMessage;
+    }
+
+    public void choosePastry() {
+        if (od > 2) {
+            chooseIntensePastry();
+        } else {
+            chooseMildPastry();
+        }
+    }
+
+    public void chooseIntensePastry() {
+        if (flavor == "chocolate") {
+            pastryName = "Death by Chocolate";
+        } else if (flavor == "vanilla" ) {
+            pastryName = "Foot Long Eclair";
+        } else if (flavor == "cherry" ) {
+            pastryName = "Mulled Cherry Bomb Pie";
+            /* https://www.halfbakedharvest.com/mulled-cherry-bomb-pie/ */
+        } else if (flavor == "lemon" ) {
+            pastryName = "Limoncello Lemon Bar";
+        }
+    }
+
+    public void chooseMildPastry() {
+        if (flavor == "chocolate") {
+            pastryName = "Baby Chocolate Puff";
+        } else if (flavor == "vanilla" ) {
+            pastryName = "Sugar Cookie";
+        } else if (flavor == "cherry" ) {
+            pastryName = "Mini Cherry Turnover";
+        } else if (flavor == "lemon" ) {
+            pastryName = "Lemon Tartlet";
+        }
+    }
+
+    public void createMessage() {
+        pastryMessage = "Sweet - your pastry personality is:" + pastryName + "!";
+        if (nuts) {
+            pastryMessage += "\n Always fun, never nuts!";
+        }
+    }
+
+    public void run() {
+        choosePastry();
+        createMessage();
     }
 
 }
